d5cdb3b07a24e155e917cb59884ffb5f4bc5de2f
diegxherrera <diegoherred@gmail.com>
2024-10-31 13:27:44 +0100

    porfi

--- components/custom/multimodal-input.tsx
+++ components/custom/multimodal-input.tsx
@@ -146,7 +146,7 @@ export function MultimodalInput({
   );
 
   return (
-      <div className="relative w-full flex flex-col gap-4 bg-transparent">
+      <div className="relative w-full flex flex-col gap-4">
         <input
             type="file"
             className="fixed -top-4 -left-4 size-0.5 opacity-0 pointer-events-none"
